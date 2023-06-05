8bc4a8da1e9c870b345b7dad1c7425f42f8b73b5
laranotreallycroft <67145166+laranotreallycroft@users.noreply.github.com>
2023-06-05 02:39:46 +0200

    neki unused import

--- BE/src/main/java/com/travelApp/travelApp/controller/SightseeingController.java
+++ BE/src/main/java/com/travelApp/travelApp/controller/SightseeingController.java
@@ -1,27 +1,14 @@
 package com.travelApp.travelApp.controller;
 
 import java.net.URISyntaxException;
-import java.util.List;
 
-import org.locationtech.jts.geom.Coordinate;
-import org.locationtech.jts.geom.GeometryFactory;
-import org.locationtech.jts.geom.Point;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.travelApp.travelApp.model.PackingList;
-import com.travelApp.travelApp.model.Trip;
-import com.travelApp.travelApp.model.User;
-import com.travelApp.travelApp.model.payload.PackingListPayload;
 import com.travelApp.travelApp.model.payload.SightseeingRoutePayload;
-import com.travelApp.travelApp.model.payload.TripPayload;
 import com.travelApp.travelApp.repository.PackingListRepository;
 import com.travelApp.travelApp.repository.TripRepository;
 import com.travelApp.travelApp.repository.UserRepository;
