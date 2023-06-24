8dbcad238cf539240e87ed3f142027e5d02d8233
laranotreallycroft <67145166+laranotreallycroft@users.noreply.github.com>
2023-06-24 22:55:58 +0200

    još jedna godina weather

--- FE/src/components/trip/weather/WeatherView.tsx
+++ FE/src/components/trip/weather/WeatherView.tsx
@@ -43,6 +43,7 @@ const WeatherView: React.FC<IWeatherViewProps> = (props: IWeatherViewProps) => {
               { value: 2021, label: "2021" },
               { value: 2020, label: "2020" },
               { value: 2019, label: "2019" },
+              { value: 2018, label: "2018" },
             ]}
             className="weatherView__select"
           />
