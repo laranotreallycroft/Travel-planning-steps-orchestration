2cabd876365c9bdb7a196d2165ce122bdf039e25
Lara Šafar Godinić <Lara.SafarGodinic@tbs.tech>
2024-11-19 20:23:54 +0100

    rename label file

--- FE/src/service/locale/message/hr.ts
+++ FE/src/service/locale/message/hr.ts
@@ -1,27 +1,3 @@
-const en: any = {
-  // ---------- Navigation labels
-  'NAVIGATION.INFO': 'How it works',
-  'NAVIGATION.LOGIN': 'Sign in',
-  'NAVIGATION.CREATE': 'Sign up',
-  'LOGIN_VIEW.FORM.EMAIL_MESSAGE': 'Please enter email',
-  'LOGIN_VIEW.FORM.EMAIL_PLACEHOLDER': 'Email address',
-  'LOGIN_VIEW.FORM.PASSWORD_MESSAGE': 'Please enter password',
-  'LOGIN_VIEW.FORM.PASSWORD_PLACEHOLDER': 'Password',
-  'LOGIN_VIEW.SIGN_IN': 'Sign in',
-  'LOGIN_VIEW.GOOGLE_LOGIN_FAIL': 'Unable to log in with Google',
-  'LOGIN_VIEW.OR': 'Or',
-  'LOGIN_VIEW.SIGN_UP': "Don't have an account? Create one.",
-  'REGISTRATION_VIEW.PASSWORD_VALIDATION.REJECT': 'Your password must be at least 8 characters including a lowercase letter, an uppercase letter, and a number',
-  'REGISTRATION_VIEW.PASSWORD_VALIDATION.NOT_THE_SAME': 'Make sure your passwords match.',
-  'REGISTRATION_VIEW.FORM.EMAIL_MESSAGE': 'The input is not a valid e-mail.',
-  'REGISTRATION_VIEW.FORM.EMAIL_PLACEHOLDER': 'Email address',
-  'REGISTRATION_VIEW.FORM.PASSWORD_MESSAGE': 'Please enter password',
-  'REGISTRATION_VIEW.FORM.PASSWORD_PLACEHOLDER': 'Password',
-  'REGISTRATION_VIEW.FORM.CONFIRM_PASSWORD_PLACEHOLDER': 'Confirm password',
-  'REGISTRATION_VIEW.SIGN_UP': 'Sign up',
-  'REGISTRATION_VIEW.OR': 'Or',
-  'REGISTRATION_VIEW.GOOGLE_SIGN_UP_FAIL': 'Unable to sign up with Google',
-  'REGISTRATION_VIEW.LOG_IN': 'Already have an account? Sign in.',
-};
+const hr: any = {};
 
-export default en;
+export default hr;
