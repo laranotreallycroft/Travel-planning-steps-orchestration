f1552a3b79bfa2e25ed2682cdf97a0244da7280d
laranotreallycroft <Lara.SafarGodinic@tbs.tech>
2024-11-21 18:46:28 +0100

    add 404 page

--- FE/src/components/fallback/FallbackView.tsx
+++ FE/src/components/fallback/FallbackView.tsx
@@ -1,18 +1,15 @@
-import { Row } from "antd";
-import Title from "antd/es/typography/Title";
-import React from "react";
+import notFoundImage from 'asset/img/not_found_image.svg';
+import React from 'react';
 
 export interface IFallbackViewOwnProps {}
 
 type IFallbackViewProps = IFallbackViewOwnProps;
 
-const FallbackView: React.FC<IFallbackViewProps> = (
-  props: IFallbackViewProps
-) => {
+const FallbackView: React.FC<IFallbackViewProps> = (props: IFallbackViewProps) => {
   return (
-    <Row justify={"center"}>
-      <Title className="title">This page does not exist...</Title>
-    </Row>
+    <div className="fallbackView__container">
+      <img src={notFoundImage} alt="No Data" />
+    </div>
   );
 };
 
