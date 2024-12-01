6d39d9e1d5081b54a7a7fa6236df5ae42d4bfe2c
laranotreallycroft <Lara.SafarGodinic@tbs.tech>
2024-12-01 21:25:56 +0100

    fix fallback

--- FE/src/components/fallback/FallbackView.tsx
+++ FE/src/components/fallback/FallbackView.tsx
@@ -1,3 +1,4 @@
+import { Col, Row } from 'antd';
 import notFoundImage from 'asset/img/not_found_image.svg';
 import React from 'react';
 
@@ -7,9 +8,11 @@ type IFallbackViewProps = IFallbackViewOwnProps;
 
 const FallbackView: React.FC<IFallbackViewProps> = (props: IFallbackViewProps) => {
   return (
-    <div className="fallbackView__container">
-      <img src={notFoundImage} alt="No Data" />
-    </div>
+    <Row className="fallbackView__container" justify={'center'}>
+      <Col xs={18} sm={18} md={12} lg={12} xl={12} xxl={12}>
+        <img src={notFoundImage} alt="No Data" />
+      </Col>
+    </Row>
   );
 };
 
