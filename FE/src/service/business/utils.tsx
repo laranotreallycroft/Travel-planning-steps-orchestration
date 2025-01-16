9b7d041bd1dc5ae9f4afe7aa126f78f5119a7ec8
laranotreallycroft <lara.safar.godinic@gmail.com>
2025-01-16 11:59:34 +0100

    logout on 403 forbidden

--- FE/src/service/business/utils.tsx
+++ FE/src/service/business/utils.tsx
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import { LoginBusinessStore } from 'service/business/login/LoginBusinessStore';
+import { getStore } from 'service/business/RootBusinessStore';
 import AuthTokenManager from 'service/util/AuthTokenManager';
 
 // Utility to get the token
@@ -9,18 +11,31 @@ const getAuthHeaders = () => {
 
 export default class EntityApiService {
   static postEntity<T>(url: string, payload: T) {
-    return axios.post(url, payload, { headers: getAuthHeaders() });
+    return axios.post(url, payload, { headers: getAuthHeaders() }).catch(handleUnauthorizedError);
   }
 
   static getEntity<T>(url: string, payload?: T) {
-    return axios.get(url, { params: payload, headers: getAuthHeaders() });
+    return axios.get(url, { params: payload, headers: getAuthHeaders() }).catch(handleUnauthorizedError);
   }
-
   static putEntity<T>(url: string, payload: T) {
-    return axios.put(url, payload, { headers: getAuthHeaders() });
+    return axios.put(url, payload, { headers: getAuthHeaders() }).catch(handleUnauthorizedError);
   }
 
   static deleteEntity<T>(url: string, payload?: T) {
-    return axios.delete(url, { data: payload, headers: getAuthHeaders() });
+    return axios.delete(url, { data: payload, headers: getAuthHeaders() }).catch(handleUnauthorizedError);
+  }
+}
+
+// Utility function to handle 403 errors and dispatch logout action
+function handleUnauthorizedError(error: any) {
+  if (error.response && error.response.status === 403) {
+    console.log('Unauthorized Access (403)');
+    getStore().dispatch(LoginBusinessStore.actions.logout()); // Dispatch logout action
+
+    // Reject the promise with a custom error message
+    return Promise.reject('Unauthorized Access (403)');
+  } else {
+    // Propagate other errors if they are not 403
+    return Promise.reject(error);
   }
 }
